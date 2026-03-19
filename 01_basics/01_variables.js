08155a70ac8e03b979e18bb19f8e7f49076448a1
Aman Shukla <amanshukla03102005@gmail.com>
2026-03-19 18:18:36 +0000

    update

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144325
+let accountname="itisamanshukla"
+var accountcity="noida"
+accountstate="up"
+let aman;
+
+//accountId=1345 //illegal as you cant change the value of a constant
+accountname="amanshukla"
+accountcity="delhi"
+accountstate="UP"
+
+//this is a single line comment 
+/* this is a multi line comment
+u should use let for variables due to issue of block and functional scope in javascript
+*/
+console.log(accountId)
+console.table([accountname,accountcity,accountstate,aman])
+
