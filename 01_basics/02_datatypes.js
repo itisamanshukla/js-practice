3f4a8a685fc52a557ee23403c80093aa06f5f81d
Aman Shukla <amanshukla03102005@gmail.com>
2026-09-21 08:46:32 +0000

    studied about types and conversions

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -3,5 +3,18 @@
 //alert(3+3) // we are using nodejs, not browser
 
 console.log(3
-    +3)
+    +3)// code should be readable
 console.log("aman")
+// although we dont have to declare the data type as js is dynamically typed
+//number- 2 to the power 53
+// bigInt
+//string
+// boolean
+// null-standalone value
+// undefined
+// symbol- use to determine whether component is unique or not
+
+// object
+console.log(typeof null); //object 
+console.log(typeof undefined); //undefined
+
---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,23 @@
+let score="33abc";
+console.log(typeof score);
+console.log(typeof(score));
+let valueinnumber=Number(score);
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+// true->1 false->0 null->0 undefined->NaN "33abc"->NaN
+
+let isloggedin=1;
+let booleanisloggedin=Boolean(isloggedin);
+console.log(booleanisloggedin);
+
+// 1-> true 0->false ""->false "aman"->true
+
+let number=33;
+let stringnumber=String(number);
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
+
